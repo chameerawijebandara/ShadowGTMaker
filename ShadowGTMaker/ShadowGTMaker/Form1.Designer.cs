d05019999ddb46ae297609fd311690f2500eb156
Chameera Wijebandara <chameerawijebandara@gmail.com>
2015-01-30 10:40:36 +0530

    Add Completed project

---
+++ ShadowGTMaker/ShadowGTMaker/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ShadowGTMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(653, 62);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Load Directory";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(470, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(653, 108);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Refresh Directory";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(663, 386);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 445);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Ground Truth Shadow Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ShadowGTMaker/ShadowGTMaker/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ShadowGTMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String baseFolder;
+        List<String> Imgfiles = new List<string>();
+        int currentImg = 0;
+        Point currntLocation;
+        bool isPlay = false;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                baseFolder = fbd.SelectedPath;
+                textBox1.Text = fbd.SelectedPath;
+                button5_Click(sender, e);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo folder = new DirectoryInfo(baseFolder);
+            FileInfo[] files = folder.GetFiles();
+            Imgfiles.Clear();
+            // Display all the files.
+            foreach (FileInfo img in files)
+            {
+                if (img.Extension.ToLower() == ".png" ||
+                   img.Extension.ToLower() == ".jpg" ||
+                   img.Extension.ToLower() == ".gif" ||
+                   img.Extension.ToLower() == ".jpeg" ||
+                   img.Extension.ToLower() == ".bmp" ||
+                   img.Extension.ToLower() == ".tif")
+                {
+                    Imgfiles.Add(img.FullName);
+                }
+            }
+            if (Imgfiles.Count == 0)
+            {
+                MessageBox.Show("No image found..!");
+                return;
+            }
+            currentImg = 0;
+            display();
+        }
+        private void display()
+        {
+            if(currentImg>=Imgfiles.Count || currentImg < 0)
+            {
+                return;
+            }
+            string DataFileName = System.IO.Path.ChangeExtension(Imgfiles[currentImg], "txt");
+            pictureBox1.Image = Image.FromFile(Imgfiles[currentImg]);
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(DataFileName);
+                string[] data = lines[1].Split(' ');
+                currntLocation = new Point(Convert.ToInt32(data[0]), Convert.ToInt32(data[1]));
+                pictureBox1.Refresh();
+                drowLines();
+
+                data = lines[2].Split(' ');
+                Point NewLocation = new Point(Convert.ToInt32(data[0]), Convert.ToInt32(data[1]));
+
+                int ans = chechNewPoint(NewLocation);
+
+                drowNum(ans, NewLocation);
+
+            }
+            catch (FileNotFoundException e)
+            {
+                if (isPlay)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("No File");
+                    isPlay = false;
+                }
+            }
+
+            //pictureBox1.Refresh();
+
+            //MessageBox.Show("Dot Net Perls is awesome.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentImg++;
+
+            if (currentImg >= Imgfiles.Count)
+            {
+                
+                timer1.Stop();
+                isPlay = false;
+                MessageBox.Show("Go to beginig..!");
+                currentImg = 0;
+            }
+            display();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentImg--;
+
+            if (currentImg < 0)
+            {
+                MessageBox.Show("Go to end..!");
+                currentImg = Imgfiles.Count - 1;
+            }
+            display();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                currntLocation = e.Location;
+
+            }
+            pictureBox1.Refresh();
+            drowLines();
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                int ans = chechNewPoint(e.Location);
+                drowNum(ans, e.Location);
+
+                string DataFileName = System.IO.Path.ChangeExtension(Imgfiles[currentImg], "txt");
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(DataFileName, false))
+                {
+                    file.WriteLine(ans);
+                    file.WriteLine(currntLocation.X + " " + currntLocation.Y);
+                    file.WriteLine(e.Location.X + " " + e.Location.Y);
+                }
+                System.Threading.Thread.Sleep(100);
+                button1_Click(null, null);
+            }
+
+        }
+        private void drowNum(int num, Point location)
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                g.FillRectangle(Brushes.Black, location.X, location.Y, 20, 20);
+                g.DrawString(" " + num, new Font("Arial", 14), Brushes.Yellow, location);
+
+            }
+        }
+        private void drowLines()
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                // Draw next line and...
+                g.DrawLine(Pens.Black, currntLocation, currntLocation);
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X, 0));
+
+                int min = Math.Min(pictureBox1.Width - currntLocation.X, currntLocation.Y);
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X + min, currntLocation.Y - min));
+
+                g.DrawLine(Pens.Black, currntLocation, new Point(pictureBox1.Width, currntLocation.Y));
+
+                min = Math.Min(pictureBox1.Width - currntLocation.X, pictureBox1.Height - currntLocation.Y);
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X + min, currntLocation.Y + min));
+
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X, pictureBox1.Height));
+
+                min = Math.Min(currntLocation.X, pictureBox1.Height - currntLocation.Y);
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X - min, currntLocation.Y + min));
+
+                g.DrawLine(Pens.Black, currntLocation, new Point(0, currntLocation.Y));
+
+                min = Math.Min(currntLocation.X, currntLocation.Y);
+                g.DrawLine(Pens.Black, currntLocation, new Point(currntLocation.X - min, currntLocation.Y - min));
+
+            }
+        }
+
+        private int chechNewPoint(Point NewLocation)
+        {
+            float m = ((float)NewLocation.Y - currntLocation.Y) / ((float)NewLocation.X - currntLocation.X);
+
+            double ang = Math.Atan(m);
+            //MessageBox.Show(" "+ang);    
+
+            if (currntLocation.Y > NewLocation.Y)
+            {
+                if (ang > 1)
+                {
+                    return 8;
+                }
+                if (ang > 0)
+                {
+                    return 7;
+                }
+
+                if (ang < -1)
+                {
+                    return 1;
+                }
+                return 2;
+            }
+            else
+            {
+                if (ang > 1)
+                {
+                    return 4;
+                }
+                if (ang > 0)
+                {
+                    return 3;
+                }
+
+                if (ang < -1)
+                {
+                    return 5;
+                }
+                return 6;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isPlay = true;
+            timer1.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
